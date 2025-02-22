a31bc38730bb756685f1c4d0d4e676d5f2b311e8
dyaksa <diasnour0395@gmail.com>
2025-02-22 07:32:03 +0700

    chore: config gracefuly shutdown mechanism

--- cmd/main.go
+++ cmd/main.go
@@ -3,9 +3,13 @@ package main
 import (
 	"context"
 	"fmt"
+	"os"
+	"os/signal"
+	"syscall"
 
 	"github.com/dyaksa/boilerplate-go-clean-arhictecture/api/route"
 	"github.com/dyaksa/boilerplate-go-clean-arhictecture/bootstrap"
+	"github.com/dyaksa/boilerplate-go-clean-arhictecture/pkg/log"
 	"github.com/fasthttp/router"
 	"github.com/valyala/fasthttp"
 )
@@ -21,5 +25,27 @@ func main() {
 	r := router.New()
 	route.Setup(r)
 
-	fasthttp.ListenAndServe(fmt.Sprintf(":%s", env.Port), r.Handler)
+	server := &fasthttp.Server{
+		Handler: r.Handler,
+	}
+
+	go func() {
+		app.Log.Info(fmt.Sprintf("%s is running on port %s", env.AppName, env.Port))
+		if err := server.ListenAndServe(fmt.Sprintf(":%s", env.Port)); err != nil {
+			app.Log.Error("failed to start server", log.Error("error", err))
+		}
+	}()
+
+	quit := make(chan os.Signal, 1)
+	signal.Notify(quit, syscall.SIGINT, syscall.SIGTERM)
+	<-quit
+
+	app.Log.Info("shutting down server")
+
+	if err := server.Shutdown(); err != nil {
+		app.Log.Error("failed to shutdown server", log.Error("error", err))
+	}
+
+	app.Log.Info("server stopped")
+
 }
